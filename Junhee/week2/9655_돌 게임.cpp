8c0075d04c88db6fbbbd8a3467f15e33878f5fc8
JunHee <ljh19980823@gmail.com>
2024-03-19 16:33:44 +0900

    9655_돌 게임.cpp

---
+++ Junhee/week2/9655_돌 게임.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int dp[1001];
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 1;
+
+	for (int i = 4; i <= n; i++) {
+		dp[i] = min(dp[i - 1], dp[i - 3]) + 1;
+	}
+
+	if (dp[n] % 2 == 1) cout << "SK";
+	else cout << "CY";
+
+	return 0;
+}
