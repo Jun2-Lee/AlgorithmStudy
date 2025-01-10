bf0b97e73c134b41bfc0369dec710efd187f103c
JunHee <ljh19980823@gmail.com>
2025-01-10 23:21:42 +0900

    12919_A 와 B 2.cpp

--- Junhee/ssafy_january/12919_A 와 B 2.cpp
+++ Junhee/ssafy_january/12919_A 와 B 2.cpp
@@ -8,18 +8,16 @@ string s, t;
 bool answer = false;
 
 void dfs(string temp) {
-    if (temp.size() < s.size()) return; // 탐색 종료 조건 (s보다 짧아지면 불가능)
+    if (temp.size() < s.size()) return;
     if (temp == s) { 
         answer = true;
         return;
     }
 
-    // 'A'로 끝나는 경우 'A' 제거
     if (temp[temp.size() - 1] == 'A') {
         dfs(temp.substr(0, temp.size() - 1));
     }
 
-    // 'B'로 시작하는 경우 뒤집고 'B' 제거
     if (temp[0] == 'B') {
         reverse(temp.begin(), temp.end());
         dfs(temp.substr(0, temp.size() - 1));
@@ -29,5 +27,6 @@ void dfs(string temp) {
 int main() {
     cin >> s >> t;
     dfs(t);
-    cout << (answer ? 1 : 0);
+    if(answer) cout << 1;
+    else cout << 0;
 }
